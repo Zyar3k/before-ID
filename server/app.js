29a0e6b8c6eba07de48517cef9e4124206347bd0
zyar3k <a.zygan@gmail.com>
2022-01-13 12:03:32 +0100

    Init server

---
+++ server/app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8888;
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+// extra packages
+
+// routes
+app.get("/", (req, res) => {
+  res.send("books api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
