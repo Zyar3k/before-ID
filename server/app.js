66429c051feb8e8082d09f0052d79032b8fb6f45
zyar3k <a.zygan@gmail.com>
2022-01-13 12:40:10 +0100

    Add pattern controllers/route

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8888;
 
+// routes
+const authRouter = require("./routes/auth");
+const booksRouter = require("./routes/books");
+
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -15,6 +19,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("books api");
 });
+
+app.use("/api/auth", authRouter);
+app.use("/api/books", booksRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ server/controllers/books.js
@@ -0,0 +1,27 @@
+const getAllBooks = async (req, res) => {
+  res.send("get all books");
+};
+
+const getBook = async (req, res) => {
+  res.send("get book");
+};
+
+const createBook = async (req, res) => {
+  res.send("create book");
+};
+
+const updateBook = async (req, res) => {
+  res.send("update book");
+};
+
+const deleteBook = async (req, res) => {
+  res.send("delete book");
+};
+
+module.exports = {
+  createBook,
+  getAllBooks,
+  getBook,
+  updateBook,
+  deleteBook,
+};
