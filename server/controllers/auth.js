13b2d29d8e8069415301c0395b5eb345f6229340
zyar3k <a.zygan@gmail.com>
2022-01-13 12:54:49 +0100

    Hash password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,17 @@
 const User = require("../models/User.js");
 const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-  const newUser = await User.create({ ...req.body });
+  const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name, email, password: hashedPassword };
+
+  const newUser = await User.create({ ...tempUser });
   res.status(StatusCodes.CREATED).json(newUser);
 };
 
